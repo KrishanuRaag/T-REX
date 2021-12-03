494a0eb40c679205084771e4027d73224ea29b9e
KrishanuRaag <95421691+KrishanuRaag@users.noreply.github.com>
2021-12-03 18:39:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+
+var trex ,trex_running;
+function preload(){
+  
+
+}
+
+function setup(){
+  createCanvas(600,200)
+ball=createSprite(50,100,20,28)
+  //create a trex sprite
+ 
+}
+
+function draw(){
+  background("pink")
+  
+drawSprites()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
